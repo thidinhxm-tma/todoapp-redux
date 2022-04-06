9ff735f0f5cd86fbcab908019de2c98c59e699d6
thidinhxm <thidinhxm@gmail.com>
2022-04-06 17:03:51 +0700

    fix file name

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,14 +1,7 @@
 import { combineReducers  } from "redux";
 
-import filtersReducer from "../components/Filters/filtersSlide";
-import todoListReducer from "../components/TodoList/todoListSlide";
-
-// const rootReducer = (state = {}, action) => {
-//   return {
-//     filters: filtersReducer(state.filters, action),
-//     todoList: todoListReducer(state.todoList, action)
-//   }
-// }
+import filtersReducer from "../components/Filters/filtersSlice";
+import todoListReducer from "../components/TodoList/todoListSlice";
 
 
 const rootReducer = combineReducers({
